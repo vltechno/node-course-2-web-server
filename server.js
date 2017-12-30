f5f75c2e0b3168ef2acae3a74929dc73778c560c
vltechno <vltechno@outlook.com>
2017-12-30 10:46:07 +0700

    Initial

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+hbs.registerHelper('curYear',() => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(msg) => {
+  return msg.toUpperCase();
+});
+// register a middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+  });
+  next();
+});
+
+// if we dont want to notice user about maintenance, will command out
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+//   // we dont use next here, so the following line will not be fired
+// });
+
+// exec from top to bottom
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs',{
+    pageTitle: 'Home Page, please use: git-scm.com for how to use git'
+    ,msg: 'welcom to my websites'
+  })
+});0
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
